f85341a41aee4b194df43d916daa74ff2a0c428a
XiaotianC <157281571+xiaotian7196@users.noreply.github.com>
2024-11-24 20:24:07 +0800

    24.11.24 [NEW] Time.js文件优化

--- assets/js/time.js
+++ assets/js/time.js
@@ -1,25 +1,28 @@
 /*
     * @Author: Xiaotian7196
-    * @Date: 2024-06-6 9:39:15
+    * @Date: 2024-11-22 11:00:50
 */
-var datetime = new Date();
-var datelocal = new Date().getTimezoneOffset();
-var timezone = -datelocal;
+let datetime = new Date();
+let DateLocal = new Date().getTimezoneOffset();
+let timezone = -DateLocal;
 console.log(datetime)
+console.log(timezone)
 document.getElementById("time").textContent = datetime;
 document.getElementById("timezone").textContent = timezone / 60;
+
 function refreshTime() {
     const timeDisplay = document.getElementById("time");
     const dateString = new Date().toLocaleString();
-    const formattedString = dateString.replace(",", "-");
-    timeDisplay.textContent = formattedString;
+    timeDisplay.textContent = dateString.replace(",", "-");
 }
+
 setInterval(refreshTime, 1000);
+
 function secondToDate(second) {
     if (!second) {
         return 0;
     }
-    var time = new Array(0, 0, 0, 0, 0);
+    let time = [0, 0, 0, 0, 0];
     if (second >= 365 * 24 * 3600) {
         time[0] = parseInt(second / (365 * 24 * 3600));
         second %= 365 * 24 * 3600;
@@ -42,30 +45,16 @@ function secondToDate(second) {
     return time;
 }
 
-function setTime() {
+function time() {
     try {
-        var create_time = Math.round(new Date(Date.UTC(2024, 1, 21, 0, 0, 0)).getTime() / 1000);
-        var timestamp = Math.round((new Date().getTime() + 8 * 60 * 60 * 1000) / 1000);
-        currentTime = secondToDate((timestamp - create_time));
-        currentTimeHtmlCN = currentTime[0] + '年' + currentTime[1] + '天'
-            + currentTime[2] + '时' + currentTime[3] + '分' + currentTime[4]
-            + '秒';
-        document.getElementById("htmer_time").innerHTML = currentTimeHtmlCN;
+        let create_time = Math.round(new Date(Date.UTC(2024, 3, 21, 0, 0, 0)).getTime() / 1000);
+        let timestamp = Math.round((new Date().getTime() + 8 * 60 * 60 * 1000) / 1000);
+        let currentTime = secondToDate((timestamp - create_time));
+        document.getElementById("html_time").innerHTML = currentTime[0] + '年' + currentTime[1] + '天' + currentTime[2] + '时' + currentTime[3] + '分' + currentTime[4] + '秒';
+    } catch (err) {
     }
-    catch (err) { }
 }
-setInterval(setTime, 1000);
-function setTimeEN() {
-    try {
-        var create_time = Math.round(new Date(Date.UTC(2024, 1, 21, 0, 0, 0)).getTime() / 1000);
-        var timestamp = Math.round((new Date().getTime() + 8 * 60 * 60 * 1000) / 1000);
-        currentTime = secondToDate((timestamp - create_time));
-        currentTimeHtmlENG = currentTime[0] + ' YEAR ' + currentTime[1] + ' DAY '
-            + currentTime[2] + ' HOUR ' + currentTime[3] + ' MIN ' + currentTime[4]
-            + ' SEC ';
-        document.getElementById("htmer_time_ENG").innerHTML = currentTimeHtmlENG;
-    }
-    catch (err) { }
-}
-setInterval(setTimeEN, 1000);
+
+setInterval(time, 1000);
+// 你管我用try，我就用！
 
